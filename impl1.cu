3819ac8b4ece5a85585ef81c3d2e944270ad98f0
ashish-17 <ashishj1707@gmail.com>
2017-04-08 17:07:52 -0400

    Shared mem impl1 one small bug remaining

--- impl1.cu
+++ impl1.cu
@@ -16,7 +16,7 @@ __global__ void pulling_kernel(GraphEdge_t* edges, uint nEdges, uint* d_curr, ui
 	int warpId = threadId / 32;
 	int beg = nEdgesPerWarp*warpId;
 	int end = beg + nEdgesPerWarp - 1;
-	
+
 	uint src = 0;
 	uint dest = 0;
 	uint weight = 0;
@@ -27,7 +27,7 @@ __global__ void pulling_kernel(GraphEdge_t* edges, uint nEdges, uint* d_curr, ui
 		src = edge->src;
 		dest = edge->dest;
 		weight = edge->weight;
-	
+
 		tmp = d_prev[src] + weight;
 		if (tmp < d_prev[dest]) {
 			atomicMin(&d_curr[dest], tmp);
@@ -44,56 +44,71 @@ __device__ uint min_dist(uint val1, uint val2) {
 	}
 }
 
-__global__ void pulling_kernel_smem(GraphEdge_t* edges, uint nEdges, uint* d_curr, uint* d_prev, int* is_changed) {
-	
+__global__ void pulling_kernel_smem(GraphEdge_t* edges, uint nEdges, uint nVertices, uint* d_curr, uint* d_prev, int* is_changed) {
+
 	__shared__ uint shared_mem[2048];
 
+	const int nEdgesPerWarp = 64;
 	int threadId = blockDim.x * blockIdx.x + threadIdx.x;
 	int threadCount = blockDim.x * gridDim.x;
 	int lane = threadId & (32 - 1);
 	int countWarps = threadCount % 32 ? threadCount / 32 + 1 : threadCount / 32;
-	int nEdgesPerWarp = nEdges % countWarps == 0 ? nEdges / countWarps : nEdges / countWarps + 1;
-	int warpId = threadId / 32;
-	int beg = nEdgesPerWarp*warpId;
-	int end = beg + nEdgesPerWarp - 1;
-	
+	int nEdgesPerIter = countWarps * nEdgesPerWarp;
+	int nIters = nEdges % nEdgesPerIter ? nEdges / nEdgesPerIter + 1 : nEdges / nEdgesPerIter;
+
 	uint src = 0;
 	uint dest = 0;
 	uint weight = 0;
-	uint tmp = 0;
 	GraphEdge_t *edge;
-	for (int i = beg + lane; i<= end && i< nEdges; i += 32) {
-		edge = edges + i;
-		src = edge->src;
-		dest = edge->dest;
-		weight = edge->weight;
-	
-		shared_mem[threadIdx.x] = min_dist(d_prev[src] + weight, d_prev[dest]);
+	for (int iter = 0; iter < nIters; ++iter) {
+		int warpId = threadId / 32 + iter*countWarps;
+		int beg = warpId * nEdgesPerWarp;
+		int end = beg + nEdgesPerWarp -1;
 
-		__syncthreads();
-		
-		if ((lane >= 1) && (edges[i].dest == edges[i-1].dest)) {
-			shared_mem[threadIdx.x] = min_dist(shared_mem[threadIdx.x], shared_mem[threadIdx.x-1]);
-		}
-		if (lane >= 2 && edges[i].dest == edges[i-2].dest) {
-			shared_mem[threadIdx.x] = min_dist(shared_mem[threadIdx.x], shared_mem[threadIdx.x-2]);
-		}
-		if (lane >= 4 && edges[i].dest == edges[i-4].dest) {
-			shared_mem[threadIdx.x] = min_dist(shared_mem[threadIdx.x], shared_mem[threadIdx.x-4]);
-		}
-		if (lane >= 8 && edges[i].dest == edges[i-8].dest) {
-			shared_mem[threadIdx.x] = min_dist(shared_mem[threadIdx.x], shared_mem[threadIdx.x-8]);
-		}
-		if (lane >= 16 && edges[i].dest == edges[i-16].dest) {
-			shared_mem[threadIdx.x] = min_dist(shared_mem[threadIdx.x], shared_mem[threadIdx.x-16]);
-		}
-		
-		__syncthreads();
+		for (int i = beg + lane; i<= end && i< nEdges; i += 32) {
+			edge = edges + i;
+			src = edge->src;
+			dest = edge->dest;
+			weight = edge->weight;
 
-		if ((beg == end) || ((i < nEdges - 1) && (i < end) && edges[i].dest != edges[i+1].dest) || (i % 32 == 31 || i == nEdges - 1 || i == end)) {
-			tmp = atomicMin(&d_curr[dest], shared_mem[threadIdx.x]);
-			if (tmp == shared_mem[threadIdx.x])
-				*is_changed = 1;
+			shared_mem[threadIdx.x] = min_dist(d_prev[src] + weight, d_prev[dest]);
+
+			__syncthreads();
+
+			if ((lane >= 1) && (edges[i].dest == edges[i-1].dest)) {
+				shared_mem[threadIdx.x] = min_dist(shared_mem[threadIdx.x], shared_mem[threadIdx.x-1]);
+			}
+			if (lane >= 2 && edges[i].dest == edges[i-2].dest) {
+				shared_mem[threadIdx.x] = min_dist(shared_mem[threadIdx.x], shared_mem[threadIdx.x-2]);
+			}
+			if (lane >= 4 && edges[i].dest == edges[i-4].dest) {
+				shared_mem[threadIdx.x] = min_dist(shared_mem[threadIdx.x], shared_mem[threadIdx.x-4]);
+			}
+			if (lane >= 8 && edges[i].dest == edges[i-8].dest) {
+				shared_mem[threadIdx.x] = min_dist(shared_mem[threadIdx.x], shared_mem[threadIdx.x-8]);
+			}
+			if (lane >= 16 && edges[i].dest == edges[i-16].dest) {
+				shared_mem[threadIdx.x] = min_dist(shared_mem[threadIdx.x], shared_mem[threadIdx.x-16]);
+			}
+
+			__syncthreads();
+
+			if (((i < nEdges - 1) && edges[i].dest != edges[i+1].dest) || (i % 32 == 31 || i == nEdges - 1)) {
+				if (d_curr[dest] > shared_mem[threadIdx.x])
+					atomicMin(&d_curr[dest], shared_mem[threadIdx.x]);
+			}
+		}
+	}
+
+	__syncthreads();
+
+	int nVerticesPerWarp = nVertices % countWarps == 0 ? nVertices / countWarps : nVertices / countWarps + 1;
+	int warpId = threadId / 32;
+	int beg = nVerticesPerWarp*warpId;
+	int end = beg + nVerticesPerWarp - 1;
+	for (int i = beg + lane; i<= end && i< nVertices && *is_changed == 0; i += 32) {
+		if (d_prev[i] != d_curr[i]) {
+			*is_changed = 1;
 		}
 	}
 }
@@ -119,13 +134,13 @@ void puller(GraphEdge_t* edges, uint nEdges, uint nVertices, uint* distance, int
 
 	for (int i = 0; i < nVertices-1; ++i) {
 		cudaMemset(d_is_changed, 0, sizeof(int));
-		
+
 		if (isIncore == 1)
 			pulling_kernel<<<bcount, bsize>>>(d_edges, nEdges, d_distances_curr, d_distances_curr, d_is_changed);
 		else if (useSharedMem == 0)
 			pulling_kernel<<<bcount, bsize>>>(d_edges, nEdges, d_distances_curr, d_distances_prev, d_is_changed);
 		else
-			pulling_kernel_smem<<<bcount, bsize>>>(d_edges, nEdges, d_distances_curr, d_distances_prev, d_is_changed);
+			pulling_kernel_smem<<<bcount, bsize>>>(d_edges, nEdges, nVertices, d_distances_curr, d_distances_prev, d_is_changed);
 
 		cudaDeviceSynchronize();
 		if (isIncore == 0)
@@ -134,16 +149,16 @@ void puller(GraphEdge_t* edges, uint nEdges, uint nVertices, uint* distance, int
 		count_iterations++;
 
 		cudaMemcpy(&h_is_changed, d_is_changed, sizeof(int), cudaMemcpyDeviceToHost);
-		if (h_is_changed == 0 || count_iterations == 20) {
+		if (h_is_changed == 0) {
 			break;
 		}
 
 	}
-	
+
 	std::cout << "Took "<<count_iterations << " iterations " << getTime() << "ms.\n";
-	
+
 	cudaMemcpy(distance, d_distances_curr, sizeof(uint)*nVertices, cudaMemcpyDeviceToHost);
-	
+
 	cudaFree(d_edges);
 	cudaFree(d_distances_curr);
 	cudaFree(d_distances_prev);
