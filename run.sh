582ff727c32ba8bdeb3493928741e74d568424f6
ashish-17 <ashishj1707@gmail.com>
2017-04-11 20:05:18 -0400

    All stats successfull

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-./sssp --input test_graphs/msdoor.txt --bsize 1024 --bcount 2 --output output.txt --method bmf --usesmem no --sync incore --sort src
+./sssp --input test_graphs/msdoor.txt --bsize 1024 --bcount 2 --output output1.txt --method bmf --usesmem no --sync incore --sort src
 
